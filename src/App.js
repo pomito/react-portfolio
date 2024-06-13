4499fc5b6d23704561a66edf1eb7dce682e1afca
Sebastian Pomiklo <spomiklo@gmail.com>
2024-06-13 19:19:10 +0200

    add(link): add LinkedIn profile link

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://test.spomiklo.dev"
+          href="https://www.linkedin.com/in/spomiklo/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Redirect to test.spomiklo.dev
+          Link to my LinkedIn profile
         </a>
       </header>
     </div>
